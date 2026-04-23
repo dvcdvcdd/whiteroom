2ba0cd398ce0eda468ff5017e5a1eb244a789ad9
Muhammad Anjas <anjaszkr@gmail.com>
2026-04-23 14:53:48 -0700

    feat: animasi transisi halaman

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import PageTransition from './components/layout/PageTransition'
 import Beranda from './pages/Beranda'
 import Filosofi from './pages/Filosofi'
 import Evaluasi from './pages/Evaluasi'
@@ -8,20 +10,30 @@ import EvaluasiTes from './pages/EvaluasiTes'
 import Statistik from './pages/Statistik'
 import NotFound from './pages/404'
 
+function AnimatedRoutes() {
+  const location = useLocation()
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageTransition><Beranda /></PageTransition>} />
+        <Route path="/filosofi" element={<PageTransition><Filosofi /></PageTransition>} />
+        <Route path="/evaluasi" element={<PageTransition><Evaluasi /></PageTransition>} />
+        <Route path="/evaluasi/:kategori" element={<PageTransition><EvaluasiTes /></PageTransition>} />
+        <Route path="/statistik" element={<PageTransition><Statistik /></PageTransition>} />
+        <Route path="*" element={<PageTransition><NotFound /></PageTransition>} />
+      </Routes>
+    </AnimatePresence>
+  )
+}
+
 export default function App() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<Beranda />} />
-            <Route path="/filosofi" element={<Filosofi />} />
-            <Route path="/evaluasi" element={<Evaluasi />} />
-            <Route path="/evaluasi/:kategori" element={<EvaluasiTes />} />
-            <Route path="/statistik" element={<Statistik />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <AnimatedRoutes />
         </main>
         <Footer />
       </div>
